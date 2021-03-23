084764f69fa92c4fa98a2303d6d7663c3030667e
legossol <wkdgothf@gmail.com>
2021-03-23 14:12:21 +0900

    Builder

---
+++ be/demo/src/main/java/com/example/demo/sec/service/AuthServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.sec.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.cmm.service.AbstractService;
+import com.example.demo.uss.domain.User;
+
+@Service
+public class AuthServiceImpl implements AuthService{
+
+	@Override
+	public int insertMany() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
